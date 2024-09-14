6ad64a459afc1838505d993eb30e31b8f45107db
hjeijinabil <145860327+hjeijinabil@users.noreply.github.com>
2024-09-14 23:57:04 +0100

    Delete backEnd/backEnd directory

--- backEnd/backEnd/src/main/java/com/reservation/reservationEnLigne/Repository/EmployéeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.reservation.reservationEnLigne.Repository;
-
-import com.reservation.reservationEnLigne.Entity.Employée;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployéeRepository extends JpaRepository<Employée,Long> {
-}
